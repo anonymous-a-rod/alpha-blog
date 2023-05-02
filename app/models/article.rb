b875e21bef7626a5efe4b1f6a0d7a4ce30235284
Aaron <32913725+anonymous-a-rod@users.noreply.github.com>
2023-05-02 14:29:52 -0400

    bootsrapped

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+  validates :description, presence: true, length: { minmum: 10, maximum: 300 }
+  
 end
