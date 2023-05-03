ea83adc202c9bf92aca24f02767c9344c0508b9c
Aaron <32913725+anonymous-a-rod@users.noreply.github.com>
2023-05-03 09:57:01 -0400

    users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :articles
-  root 'pages#home'
+
+  resources :users, except: %i[ new ]
+  get 'signup', to: 'users#new'
+  
   get 'about', to: 'pages#about'
+  
+  root 'pages#home'
+
 end
