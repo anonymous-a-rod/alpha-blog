47546279abf2f9b70cf1c36ac3043779fbfafc6e
Aaron <32913725+anonymous-a-rod@users.noreply.github.com>
2023-05-03 14:14:26 -0400

    login logout sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   resources :users, except: %i[ new ]
   get 'signup', to: 'users#new'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'about', to: 'pages#about'
   
